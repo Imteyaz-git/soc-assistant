f91237092645c28d01187b75c8273c657643fda5
Imteyaz Ali <imteyaz.br29@gmail.com>
2026-06-27 01:42:30 +0530

    Add project structure, test main.py, set up VS Code

---
+++ main.py
@@ -0,0 +1,8 @@
+# this is test to make sure vs code and python working correctly
+
+print("SOC assistant is set up successfully")
+
+print("VS code is working correctly with Python")  
+
+print ("Python is working correctly with VS code")
+
